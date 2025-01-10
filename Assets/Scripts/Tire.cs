d775bdac7d97c12f519e02eba42e38638b16a789
dorgui00 <161583707+dorgui00@users.noreply.github.com>
2025-01-10 10:31:03 +0100

    changer code tire

--- Assets/Scripts/Tire.cs
+++ Assets/Scripts/Tire.cs
@@ -19,7 +19,7 @@ public class Tire : MonoBehaviour
 
         if (_bale == 0)
         {
-            relode--;
+            relode = 6;
             Debug.Log("true");
             return true;
           
@@ -27,6 +27,27 @@ public class Tire : MonoBehaviour
         else
         {
             relode--;
+            Debug.Log("false");
+            return false;
+
+        }
+    }
+
+    bool Roullette2()
+    {
+
+        int _bale = Random.Range(0, relode);
+
+        if (_bale == 0)
+        {
+            
+            Debug.Log("true");
+            return true;
+
+        }
+        else
+        {
+            
             Debug.Log("false");
             return false;
 
